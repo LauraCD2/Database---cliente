3c0b7e0e2600efea815b743fa7ab7e6b2a28aaf0
andresOspina19 <andresospina@unisinu.edu.co>
2021-09-08 08:00:50 -0500

    changing sql files

--- src/utils/biblioteca.sql
+++ src/utils/biblioteca.sql
@@ -1,49 +1,42 @@
-CREATE TABLE `biblioteca` (
-  `bib_id` INT NOT NULL,
-  `bib_tag` CHAR(40) NOT NULL,
-  `bib_contenido` INT NOT NULL,
-  `bib_fecha` DATETIME NOT NULL,
-  PRIMARY KEY (`bib_id`),
-  INDEX `fk_biblioteca_contenido_idx` (`bib_tag` ASC) VISIBLE,
-  INDEX `fk_biblioteca_cliente1_idx` (`bib_contenido` ASC) VISIBLE,
-  CONSTRAINT `fk_biblioteca_contenido`
-    FOREIGN KEY (`bib_tag`)
-    REFERENCES `cliente` (`cli_tag`)
-,
-  CONSTRAINT `fk_biblioteca_cliente1`
-    FOREIGN KEY (`bib_contenido`)
-    REFERENCES `contenido` (`con_id`)
+CREATE TABLE biblioteca(
+	 bib_id INT NOT NULL AUTO_INCREMENT,
+     bib_tag CHAR(40) NOT NULL,
+     bib_contenido INT NOT NULL,
+     bib_fecha DATETIME NULL,
+     PRIMARY KEY(bib_id),
+     FOREIGN KEY(bib_tag) REFERENCES cliente(cli_tag) ON DELETE CASCADE,
+     FOREIGN KEY(bib_contenido) REFERENCES contenido(con_id)
 );
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (1,'hoodoo', 999, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('hoodoo', 1, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (2,'hoodoo', 333, "2020-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('hoodoo', 7, "2020-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (3,'hoodoo', 666, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('hoodoo', 4, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (4,'andre09', 666, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('andre09', 4, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (5,'holy', 666, "2018-12-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('holy', 4, "2018-12-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (6,'holy', 555, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('holy', 5, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (7,'holy', 999, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('holy', 1, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (8,'holy', 444, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('holy', 6, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (9,'jquispe', 111, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('jquispe', 9, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (10,'jk2929', 333, "2018-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('jk2929', 7, "2018-10-25 20:00:00");
 
-INSERT INTO biblioteca(bib_id, bib_tag, bib_contenido, bib_fecha)
-VALUES (11,'cgallardo', 999, "2021-10-25 20:00:00");
+INSERT INTO biblioteca(bib_tag, bib_contenido, bib_fecha)
+VALUES ('cgallardo', 1, "2021-10-25 20:00:00");
--- src/utils/consultas.sql
+++ src/utils/consultas.sql
@@ -1,47 +1,33 @@
-Select 'Consulta 1';
- Select con_titulo 
- FROM contenido
- ORDER BY con_titulo desc;
- 
- Select 'Consulta 2';
- Select con_titulo 
- FROM contenido c
- join juego j
- ON c.con_id = j.jug_id
- WHERE jug_clasificacion like "%+%";
- 
- Select 'Consulta 3';
- Select con_titulo , jug_clasificacion , jug_categoria
- FROM contenido c
- join juego j
- ON c.con_id = j.jug_id
- WHERE jug_distribuidor = 1236;
- 
- Select 'Consulta 4';
- Select con_titulo
- FROM biblioteca b
- join contenido c
- ON b.bib_contenido = c.con_id
- WHERE bib_tag like "hoodoo";
- 
- Select 'Consulta 5';
-  Select cli_nombre
- FROM biblioteca b 
- join cliente cl
- ON cl.cli_tag = b.bib_tag
-  WHERE bib_contenido = 666
- ORDER BY cli_nombre asc;
- 
- Select 'Consulta 6';
-  Select COUNT(app_tipo)
- FROM contenido c
- join aplicacion a
- ON a.app_id = c.con_id
-  WHERE app_tipo like "Música";
-  
-Select 'Consulta 7';
-  Select cli_email
-  FROM cliente
-  WHERE cli_tag like "andre09";
- 
- 
+SELECT con_titulo as "Consulta 1" FROM contenido ORDER BY con_titulo DESC;
+
+SELECT con_titulo, jug_clasificacion as "Consulta 2" 
+FROM contenido C
+JOIN juego J
+ON C.con_id = J.jug_id WHERE jug_clasificacion LIKE "%+%";
+
+SELECT con_titulo, jug_clasificacion, jug_categoria as "Consulta 3"
+FROM contenido C
+JOIN juego J
+ON C.con_id = J.jug_id WHERE jug_distribuidor = 3;
+
+SELECT con_titulo as "Consulta 4"
+FROM biblioteca B
+JOIN contenido c
+ON B.bib_contenido = c.con_id
+WHERE bib_tag LIKE "hoodoo"; 
+
+SELECT cli_nombre
+FROM cliente C
+JOIN biblioteca B
+ON C.cli_tag LIKE B.bib_tag 
+WHERE bib_contenido = 4 ORDER BY cli_nombre ASC;
+
+SELECT COUNT(app_tipo) as "Consulta 6"
+FROM contenido C
+JOIN aplicacion A
+ON C.con_id = A.app_id 
+WHERE app_tipo LIKE "Música";
+
+SELECT cli_email FROM cliente WHERE cli_tag LIKE "andre09";
+
+ SELECT User, Host FROM mysql.user; 
